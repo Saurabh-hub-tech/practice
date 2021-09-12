13e0ccd0d0221df7fc63040f613868add92400a4
Admin <Admin@Admin-PC>
2021-09-12 11:57:45 +0530

    My First commit...:)

---
+++ src/Test/Sample.java
@@ -0,0 +1,27 @@
+package Test;
+
+public class Sample {
+	
+	// int a, b;
+	
+	 Sample (int x,int y) {
+		
+		int c=x+y;
+		
+		System.out.println("Sum is " +c);
+
+	}
+	
+	 
+	
+
+	public static void main(String[] args) {
+
+		Sample s=new Sample(5,1);
+		
+		
+		
+		
+	}
+
+}
